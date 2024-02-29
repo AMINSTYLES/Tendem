2af3eeefca5007a13ec1c4fe214496be1383313c
AMINSTYLES <mystylesamin@gmail.com>
2024-02-29 04:21:18 +0000

    God is great

--- new.sh
+++ new.sh
@@ -3,4 +3,9 @@ What is your name?
 My name is John.
 Nice meeting you John.How can I be of help?
 You will find out soon. ok
+<<<<<<< Updated upstream
 I want you to install my apache in my mobaxterm.
+=======
+nothing is had for God to do
+>>>>>>> Stashed changes
+indeed, he is lord
